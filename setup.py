ddc53ace6d8197f4940908717ae9c6e5a9ffd24e
Daniel Pope <mauve@mauveweb.co.uk>
2017-10-18 19:22:32 +0100

    Bump version and add description

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 
 setup(
     name='lightvolume',
-    version='0.1.post1',
+    version='0.1.1',
     author='Daniel Pope',
     author_email='mauve@mauveweb.co.uk',
+    description="Light volume calculation and rendering with OpenGL",
+    long_description=open('README.rst').read(),
     url='https://github.com/lordmauve/lightvolume',
     py_modules=['lightvolume'],
     install_requires=[
