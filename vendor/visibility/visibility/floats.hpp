57189e04e4d0d4c2fbba82e023a334316a789693
Daniel Pope <mauve@mauveweb.co.uk>
2017-10-22 19:26:08 +0100

    Fix for MSVC build error?

--- vendor/visibility/visibility/floats.hpp
+++ vendor/visibility/visibility/floats.hpp
@@ -6,16 +6,19 @@
 
 #include "vector2.hpp"
 
+using namespace std;
+
+
 namespace geometry
-{ 
+{
     inline bool approx_equal(float a, float b, float epsilon = std::numeric_limits<float>::epsilon())
     {
-        return std::abs(a - b) <= std::max(std::abs(a), std::abs(b)) * epsilon;
+        return abs(a - b) <= max(abs(a), abs(b)) * epsilon;
     }
 
     inline bool strictly_less(float a, float b, float epsilon = std::numeric_limits<float>::epsilon())
     {
-        return (b - a) > std::max(std::abs(a), std::abs(b)) * epsilon;
+        return (b - a) > max(abs(a), abs(b)) * epsilon;
     }
 
     template<typename T>
